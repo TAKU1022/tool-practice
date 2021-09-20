a234cfdc27b6909926e03c1fe1a42c071d1e9663
Takumi <nase49946@gmail.com>
2021-09-20 22:17:38 +0900

    TaskItem完成版

--- src/components/taskItem/TaskItem.tsx
+++ src/components/taskItem/TaskItem.tsx
@@ -1,26 +1,44 @@
 import { Checkbox } from '@material-ui/core';
-import { EventNote } from '@material-ui/icons';
+import { Delete, Edit, EventNote } from '@material-ui/icons';
 import { VFC } from 'react';
 import styles from './TaskItem.module.scss';
 
 type Props = {
-  id: number;
-  title: string;
-  completed: boolean;
+  task: {
+    id: number;
+    title: string;
+    completed: boolean;
+  };
 };
 
 const TaskItem: VFC<Props> = (props: Props) => {
+  const { task } = props;
+
   return (
     <div className={styles.root}>
       <div className={styles.title}>
         <EventNote />
-        <div className={styles.title_text}>{props.title}</div>
+        <div className={styles.title_text}>{task.title}</div>
       </div>
-      <Checkbox
-        checked={checked}
-        onChange={handleChange}
-        inputProps={{ 'aria-label': 'primary checkbox' }}
-      />
+      <div className={styles.right_item}>
+        <Checkbox
+          className={styles.checkbox}
+          checked={task.completed}
+          onClick={() => console.log(`check ${task.id}`)}
+        />
+        <button
+          className={styles.edit_button}
+          onClick={() => console.log(`check ${task.id}`)}
+        >
+          <Edit className={styles.icon} />
+        </button>
+        <button
+          className={styles.delete_button}
+          onClick={() => console.log(`check ${task.id}`)}
+        >
+          <Delete className={styles.icon} />
+        </button>
+      </div>
     </div>
   );
 };
