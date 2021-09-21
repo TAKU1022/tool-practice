654fc42d7a9af3b3d3b20ade63675f239ca8482b
Takumi <nase49946@gmail.com>
2021-09-21 22:46:27 +0900

    taskSlice作成

---
+++ src/features/task/taskSlice.ts
@@ -0,0 +1,38 @@
+import { createSlice } from '@reduxjs/toolkit';
+import { RootState } from '../../app/store';
+
+export interface TaskState {
+  idCount: number;
+  tasks: { id: number; title: string; completed: boolean }[];
+  selectedTask: { id: number; title: string; completed: boolean };
+  isModalOpen: boolean;
+}
+
+const initialState: TaskState = {
+  idCount: 1,
+  tasks: [{ id: 1, title: 'Task A', completed: false }],
+  selectedTask: { id: 0, title: '', completed: false },
+  isModalOpen: false,
+};
+
+export const taskSlice = createSlice({
+  name: 'task',
+  initialState,
+  reducers: {
+    createTask: (state, action) => {
+      state.idCount++;
+      const newTask = {
+        id: state.idCount,
+        title: action.payload,
+        completed: false,
+      };
+      state.tasks = [...state.tasks, newTask];
+    },
+  },
+});
+
+export const { createTask } = taskSlice.actions;
+
+export const selectTask = (state: RootState) => state.task.tasks;
+
+export default taskSlice.reducer;
